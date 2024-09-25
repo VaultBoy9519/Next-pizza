8361d207cbbf0706c29b8ece7d21400a4edbe873
VaultBoy9519 <illidan9519@mail.ru>
2024-09-25 03:21:24 +0300

    fix search

--- app/api/products/search/route.ts
+++ app/api/products/search/route.ts
@@ -3,6 +3,6 @@ import { NextRequest, NextResponse } from 'next/server'
 
 export async function GET(req: NextRequest) {
 	const query = req.nextUrl.searchParams.get('query') || ''
-	const products = prismaControllers.product.getByCountWithQuery(query, 5)
+	const products = await prismaControllers.product.getByCountWithQuery(query, 5)
 	return NextResponse.json(products)
 }
